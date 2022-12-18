5e33a88a656478a8c0b847553359513d4d6d20c8
Irene5698 <110497689+Irene5698@users.noreply.github.com>
2022-12-19 00:27:10 +0200

    Done

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from "./gallery-items.js";
+
+console.log(galleryItems);
+
+const imagesContainer = document.querySelector(".gallery");
+const imagesMarkup = createGalleryItems(galleryItems);
+imagesContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+        </a>`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
